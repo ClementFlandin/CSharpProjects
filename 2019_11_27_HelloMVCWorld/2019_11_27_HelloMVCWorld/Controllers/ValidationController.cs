b95171d439aee51c2cad7197aee81ec55efff5d6
Clément Flandin <clement.flandin@outlook.com>
2019-12-03 20:24:47 +0100

    Model validation

---
+++ 2019_11_27_HelloMVCWorld/2019_11_27_HelloMVCWorld/Controllers/ValidationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _2019_11_27_HelloMVCWorld.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2019_11_27_HelloMVCWorld.Controllers
+{
+    public class ValidationController : Controller
+    {
+        [HttpGet]
+        public IActionResult SimpleValidation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SimpleValidation(WebUser webUser)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content("Thank you!");
+            }
+            else
+            {
+                return View(webUser);
+            }
+        }
+    }
+}
---
+++ 2019_11_27_HelloMVCWorld/2019_11_27_HelloMVCWorld/Models/WebUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2019_11_27_HelloMVCWorld.Models
+{
+    public class WebUser
+    {
+        [Required]
+        [StringLength(25)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50,MinimumLength = 3)]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string MailAddress { get; set; }
+    }
+}
