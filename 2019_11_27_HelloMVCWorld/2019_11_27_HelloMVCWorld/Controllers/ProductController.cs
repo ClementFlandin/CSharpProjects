4760989e96d46ba729025d930fc2f6d8dad607da
Clément Flandin <clement.flandin@outlook.com>
2019-12-03 11:27:30 +0100

    Adding controllers, a ViewModel and SSH key

---
+++ 2019_11_27_HelloMVCWorld/2019_11_27_HelloMVCWorld/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2019_11_27_HelloMVCWorld.Controllers
+{
+    [Route("/products")]
+    public class ProductController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return Content("Product overview");
+        }
+        [Route("{id}")]
+        public IActionResult Details (int id)
+        {
+            return Content("Product # " + id);
+        }
+    }
+}
---
+++ 2019_11_27_HelloMVCWorld/2019_11_27_HelloMVCWorld/ViewModels/AddressViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2019_11_27_HelloMVCWorld.ViewModels
+{
+    public class AddressViewModel
+    {
+        public string StreetName { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
