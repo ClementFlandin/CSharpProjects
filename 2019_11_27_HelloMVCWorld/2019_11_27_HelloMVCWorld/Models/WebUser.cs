526bdf1c0bc0e2438dd4e102663dc5e7a96c24fd
Clément Flandin <clement.flandin@outlook.com>
2019-12-05 13:39:13 +0100

    Adding model validation test, doesn't quite work yet

--- 2019_11_27_HelloMVCWorld/2019_11_27_HelloMVCWorld/Models/WebUser.cs
+++ 2019_11_27_HelloMVCWorld/2019_11_27_HelloMVCWorld/Models/WebUser.cs
@@ -11,9 +11,11 @@ namespace _2019_11_27_HelloMVCWorld.Models
         [Required]
         [StringLength(25)]
         public string FirstName { get; set; }
+
         [Required]
         [StringLength(50,MinimumLength = 3)]
         public string LastName { get; set; }
+
         [Required]
         [EmailAddress]
         public string MailAddress { get; set; }
